8cf0fff05536e8cec77b87b3e8058bf9f0f111ea
PandiyanRajendiran <pandi1331@gmail.com>
2021-07-09 22:54:32 +0530

    Update Home.test.tsx

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render } from '../../test/test-utils';
+import { render, screen } from '../../test/test-utils';
 
 import Home from './Home';
 
 it('renders <Home /> page', () => {
   // You should be able to show that you can verify Home rendered correctly
+  const { container } = render(<Home />);
+  const welcomeElement = screen.getByText(/Welcome!/i);
+  expect(welcomeElement).toBeInTheDocument();
+  expect(container).toMatchSnapshot('home-snapshot');
 });
