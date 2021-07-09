7d0bbe5e696873c6b74f8fb8afd8012eb34f089a
PandiyanRajendiran <pandi1331@gmail.com>
2021-07-09 22:56:01 +0530

    Update sample.ts

--- src/redux/actions/sample.ts
+++ src/redux/actions/sample.ts
@@ -1,3 +1,10 @@
 import * as types from '../types';
 
-export const incrementOne = { type: types.INCREMENT_ONE };
+export interface Action {
+    type:String,
+    count?: number
+};
+
+export const incrementOne:Action = { type: types.INCREMENT_ONE };
+
+export const incrementByCount = (count: number):Action => ({ count, type: types.INCREMENT_BY_COUNT });
