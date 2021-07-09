9703ed1f527d01f55f0a0729049740a6f40496ab
PandiyanRajendiran <pandi1331@gmail.com>
2021-07-09 22:53:20 +0530

    Adding LayoutContainer unit test

--- src/components/LayoutContainer.test.tsx
+++ src/components/LayoutContainer.test.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { render } from '../../test/test-utils';
+import { render, screen } from '../../test/test-utils';
 
 import LayoutContainer from './LayoutContainer';
 
 it('can browse to the about page', async () => {
   // For `LayoutContainer`, you should be able to render the layout container, followed by navigating to the About page.
+  const { container,  history: { navigate }, } = render(<LayoutContainer />);
+  const aboutElement = screen.getByText('Go to about');
+  expect(aboutElement).toBeInTheDocument();
+
+  expect(container).toMatchSnapshot('layout-snapshot');
+  await navigate('/about');
+  expect(aboutElement).not.toBeInTheDocument();
+  const aboutHeader = screen.getByText('About Page');
+  expect(aboutHeader).toBeInTheDocument();
 });
