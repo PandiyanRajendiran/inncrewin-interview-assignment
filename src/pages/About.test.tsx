f758d40ce0d327ff5f18c6c6022b54a49c1e587a
PandiyanRajendiran <pandi1331@gmail.com>
2021-07-09 22:54:12 +0530

    Update About.test.tsx

--- src/pages/About.test.tsx
+++ src/pages/About.test.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import { render } from '../../test/test-utils';
+import { render, screen, userEvent } from '../../test/test-utils';
 
 import About from './About';
 
 it('renders <About /> page', () => {
   // You should be able to verify the About page rendered correctly.
+  const { container } = render(<About />);
+  const headerElelement = screen.getByText('About Page', { exact: false});
+  expect(headerElelement).toBeInTheDocument();
+  expect(container).toMatchSnapshot('about-snapshot');
 });
 
 it('clicks button and fires increment counter', () => {
   // You should be able to click the increment button and verify the count.
+  const { container } = render(<About />);
+  
+  let countElement = screen.getByText('Current Count: 0', { exact: false});
+  expect(countElement).toBeInTheDocument();
+  
+  const buttonElement = screen.getByRole('button');
+  userEvent.click(buttonElement);
+  
+  countElement = screen.getByText('Current Count: 1', { exact: false});
+  expect(countElement).toBeInTheDocument();
 });
